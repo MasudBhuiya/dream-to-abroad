6317d9ef52585a74f767708bf6d1c0715668197e
MasudBhuiya <masudbhuiyan1415@gmail.com>
2025-02-19 01:49:49 +0600

    fix some design

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import '../Banner/Banner.css'
 const Banner = () => {
     return (
-        <div className='banner flex flex-col w-full h-[50vh] md:h-[80vh] justify-center '>
-           <div className='px-5'>
-            <h1 className='banner-header'>Move Student</h1>
-            <h1 className='banner-sub-header'>To Their Dream Country</h1>
-            <p className='banner-p'>Identify the problem and work on solving it</p>
-           <button className='btn text-white px-5  mt-12 bg-[#6757F1]'>Book a call</button>
+        <div className='banner w-full h-[50vh] md:h-[80vh]'>
+           <div className='px-5 max-w-[1440px] mt-[7vh] md:pt-[15vh] mx-auto'>
+            <h1 className='banner-header text-4xl md:text-5xl text-center md:text-start'>Move Student</h1>
+            <h1 className='banner-sub-header text-4xl md:text-5xl my-3 md:text-start'>To Their Dream Country</h1>
+            <p className='banner-p text-lg md:text-xl md:text-start'>Identify the problem and work on solving it</p>
+           <button className='btn text-white px-5 w-full md:w-fit mt-6 md:mt-12 bg-[#6757F1]'>Book a call</button>
            </div>
         </div>
     );
