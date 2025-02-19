a013b3757c77319396c0af6688c4788a9fc1d408
MasudBhuiya <masudbhuiyan1415@gmail.com>
2025-02-19 23:33:48 +0600

    added students review section

--- src/Home/Home/Home.jsx
+++ src/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../Banner/Banner';
 import Service from '../Banner/Service/Service';
 import TopCountry from '../TopCountry/TopCountry';
 import IELTS from '../IELTS/IELTS';
+import StudentsReview from '../StudentsReview/StudentsReview';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <Service></Service>
             <TopCountry></TopCountry>
             <IELTS></IELTS>
+            <StudentsReview></StudentsReview>
         </div>
     );
 };
---
+++ src/Home/StudentsReview/StudentsReview.jsx
@@ -0,0 +1,224 @@
+import React from 'react';
+
+const StudentsReview = () => {
+    return (
+        <div>
+            <div className='mt-32 max-w-[1170px] mx-auto'>
+                <p className='service'>Reviews</p>
+            <h1 className='service-header'>What our students say</h1>
+
+            <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <div className="w-full h-[700px] sm:h-[550px] md:h-96">
+
+    </div>
+    
+    <div className="absolute  transform -translate-y-1/2 left-5 right-5 top-1/2">
+      
+
+
+      <div className="grid md:grid-cols-2 gap-6 mb-4">
+                    <div className="border p-6 rounded-lg">
+                        <div className=''>
+                        <div className='flex justify-between mb-5 items-center'>
+                        <div className='flex items-center gap-6'>
+                            <img className='h-20 rounded-full border-2 border-orange-300' src="https://i.ibb.co.com/XZy0yqyP/Ellipse-1595.png" alt="" />
+                            <div>
+                                <h1 className='text-2xl font-bold'>Lisa Alina</h1>
+                                <p>Student</p>
+                            </div>
+                            
+                        </div>
+                        {/* <FaQuoteLeft className='text-6xl text-orange-300'/> */}
+                        </div>
+                        
+                            <p>I don't how this awesome person keep helping all the vulnerable students in such a amazing way but he does it very well. Thanks for all the tips which you presented after relentless research. Bravo!!!</p>
+                        
+                        </div>
+                    </div>
+                    <div className="border p-6 rounded-lg">
+                        <div className=''>
+                        <div className='flex justify-between mb-5 items-center'>
+                        <div className='flex items-center gap-6'>
+                            <img className='h-20 rounded-full border-2 border-orange-300' src="https://i.ibb.co.com/S7xd3KBM/Ellipse-1596.png" alt="" />
+                            <div>
+                                <h1 className='text-2xl font-bold'>Arif Hossain</h1>
+                                <p>Student</p>
+                            </div>
+                            
+                        </div>
+                        {/* <FaQuoteLeft className='text-6xl text-orange-300'/> */}
+                        </div>
+                        
+                            <p>I don't how this awesome person keep helping all the vulnerable students in such a amazing way but he does it very well. Thanks for all the tips which you presented after relentless research. Bravo!!!</p>
+                        
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className='flex items-center justify-center gap-2'>
+                <a href="#slide4" className="btn btn-circle btn-sm">❮</a> 
+                <a href="#slide2" className="btn btn-circle btn-sm">❯</a>
+                </div>
+    </div>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+  <div className="w-full h-[700px] sm:h-[550px] md:h-96">
+
+</div>
+    <div className="absolute  transform -translate-y-1/2 left-5 right-5 top-1/2">
+       
+      <div className="grid md:grid-cols-2 gap-6 mb-4">
+                    <div className="border p-6 rounded-lg">
+                        <div className=''>
+                        <div className='flex justify-between mb-5 items-center'>
+                        <div className='flex items-center gap-6'>
+                            <img className='h-20 rounded-full border-2 border-orange-300' src="https://i.ibb.co.com/XZy0yqyP/Ellipse-1595.png" alt="" />
+                            <div>
+                                <h1 className='text-2xl font-bold'>Riya Mony</h1>
+                                <p>Student</p>
+                            </div>
+                            
+                        </div>
+                        {/* <FaQuoteLeft className='text-6xl text-orange-300'/> */}
+                        </div>
+                        
+                            <p>Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknow printer tool a galley of type and scrambled it to make type specimen book has survived not only five centurines.</p>
+                        
+                        </div>
+                    </div>
+                    <div className="border p-6 rounded-lg">
+                        <div className=''>
+                        <div className='flex justify-between mb-5 items-center'>
+                        <div className='flex items-center gap-6'>
+                            <img className='h-20 rounded-full border-2 border-orange-300' src="https://i.ibb.co.com/S7xd3KBM/Ellipse-1596.png" alt="" />
+                            <div>
+                                <h1 className='text-2xl font-bold'>Fardin Akash</h1>
+                                <p>Student</p>
+                            </div>
+                            
+                        </div>
+                        {/* <FaQuoteLeft className='text-6xl text-orange-300'/> */}
+                        </div>
+                        
+                            <p>Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknow printer tool a galley of type and scrambled it to make type specimen book has survived not only five centurines.</p>
+                        
+                        </div>
+                    </div>
+                </div>
+                <div className='flex items-center justify-center gap-2'>
+                <a href="#slide1" className="btn btn-circle btn-sm">❮</a>
+                <a href="#slide3" className="btn btn-circle btn-sm">❯</a>
+                </div>
+    </div>
+  </div> 
+  <div id="slide3" className="carousel-item relative w-full">
+  <div className="w-full h-[700px] sm:h-[550px] md:h-96">
+
+</div>
+    <div className="absolute transform -translate-y-1/2 left-5 right-5 top-1/2">
+       
+      <div className="grid md:grid-cols-2 gap-6 mb-4">
+                    <div className="border p-6 rounded-lg">
+                        <div className=''>
+                        <div className='flex justify-between mb-5 items-center'>
+                        <div className='flex items-center gap-6'>
+                            <img className='h-20 rounded-full border-2 border-orange-300' src="https://i.ibb.co.com/XZy0yqyP/Ellipse-1595.png" alt="" />
+                            <div>
+                                <h1 className='text-2xl font-bold'>Faisa Rahi</h1>
+                                <p>Student</p>
+                            </div>
+                            
+                        </div>
+                        {/* <FaQuoteLeft className='text-6xl text-orange-300'/> */}
+                        </div>
+                        
+                            <p>Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknow printer tool a galley of type and scrambled it to make type specimen book has survived not only five centurines.</p>
+                        
+                        </div>
+                    </div>
+                    <div className="border p-6 rounded-lg">
+                        <div className=''>
+                        <div className='flex justify-between mb-5 items-center'>
+                        <div className='flex items-center gap-6'>
+                            <img className='h-20 rounded-full border-2 border-orange-300' src="https://i.ibb.co.com/S7xd3KBM/Ellipse-1596.png" alt="" />
+                            <div>
+                                <h1 className='text-2xl font-bold'>Farnand Alik</h1>
+                                <p>Student</p>
+                            </div>
+                            
+                        </div>
+                        {/* <FaQuoteLeft className='text-6xl text-orange-300'/> */}
+                        </div>
+                        
+                            <p>Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknow printer tool a galley of type and scrambled it to make type specimen book has survived not only five centurines.</p>
+                        
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex items-center justify-center gap-2'>
+                <a href="#slide2" className="btn btn-circle btn-sm">❮</a>
+      <a href="#slide4" className="btn btn-circle btn-sm">❯</a>
+                </div>
+    </div>
+  </div> 
+  <div id="slide4" className="carousel-item relative w-full">
+  <div className="w-full h-[700px] sm:h-[550px] md:h-96">
+
+</div>
+    <div className="absolute transform -translate-y-1/2 left-5 right-5 top-1/2">
+      
+      <div className="grid md:grid-cols-2 gap-6 mb-4">
+                    <div className="border p-6 rounded-lg">
+                        <div className=''>
+                        <div className='flex justify-between mb-5 items-center'>
+                        <div className='flex items-center gap-6'>
+                            <img className='h-20 rounded-full border-2 border-orange-300' src="https://i.ibb.co.com/XZy0yqyP/Ellipse-1595.png" alt="" />
+                            <div>
+                                <h1 className='text-2xl font-bold'> Alina saika</h1>
+                                <p>Student</p>
+                            </div>
+                            
+                        </div>
+                        {/* <FaQuoteLeft className='text-6xl text-orange-300'/> */}
+                        </div>
+                        
+                            <p>Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknow printer tool a galley of type and scrambled it to make type specimen book has survived not only five centurines.</p>
+                        
+                        </div>
+                    </div>
+                    <div className="border p-6 rounded-lg">
+                        <div className=''>
+                        <div className='flex justify-between mb-5 items-center'>
+                        <div className='flex items-center gap-6'>
+                            <img className='h-20 rounded-full border-2 border-orange-300' src="https://i.ibb.co.com/S7xd3KBM/Ellipse-1596.png" alt="" />
+                            <div>
+                                <h1 className='text-2xl font-bold'>Rakindra Pravan</h1>
+                                <p>Student</p>
+                            </div>
+                            
+                        </div>
+                        {/* <FaQuoteLeft className='text-6xl text-orange-300'/> */}
+                        </div>
+                        
+                            <p>Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknow printer tool a galley of type and scrambled it to make type specimen book has survived not only five centurines.</p>
+                        
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex items-center justify-center gap-2'>
+                <a href="#slide3" className="btn btn-circle btn-sm">❮</a>
+      <a href="#slide1" className="btn btn-circle btn-sm">❯</a>
+                </div>
+    </div>
+  </div>
+</div>
+        </div>
+        </div>
+    );
+};
+
+export default StudentsReview;
